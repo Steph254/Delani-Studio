12bbe2be1e928ef385ddcfdd2863343d7b276044
Stephania Mwikali <steph.mwikali@gmail.com>
2019-07-21 19:17:02 +0300

    Remove repititive code for the mouse over function

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
     $("#des").toggle();
     $("#ign").toggle();
   });
-});
+
 $(".prise").click(function() {
   $("#dev").toggle();
   $("#ment").toggle();
@@ -12,61 +12,10 @@ $(".manage").click(function() {
   $("#uct").toggle();
   $("#prod").toggle();
 });
-
-$(document).ready(function() {
-  $("#work4").mouseover(function() {
-    $(".title1").show();
+$(".work").mouseover(function() {
+    $(this).find('.title').show();
   }).mouseout(function() {
-    $(".title1").hide();
-  });
-});
-$(document).ready(function() {
-  $("#work3").mouseover(function() {
-    $(".title2").show();
-  }).mouseout(function() {
-    $(".title2").hide();
-  });
-});
-$(document).ready(function() {
-  $("#work2").mouseover(function() {
-    $(".title3").show();
-  }).mouseout(function() {
-    $(".title3").hide();
-  });
-});
-$(document).ready(function() {
-  $("#work1").mouseover(function() {
-    $(".title4").show();
-  }).mouseout(function() {
-    $(".title4").hide();
-  });
-});
-$(document).ready(function() {
-  $("#work5").mouseover(function() {
-    $(".title5").show();
-  }).mouseout(function() {
-    $(".title5").hide();
-  });
-});
-$(document).ready(function() {
-  $("#work6").mouseover(function() {
-    $(".title6").show();
-  }).mouseout(function() {
-    $(".title6").hide();
-  });
-});
-$(document).ready(function() {
-  $("#work7").mouseover(function() {
-    $(".title7").show();
-  }).mouseout(function() {
-    $(".title7").hide();
-  });
-});
-$(document).ready(function() {
-  $("#work8").mouseover(function() {
-    $(".title8").show();
-  }).mouseout(function() {
-    $(".title8").hide();
+    $(this).find('.title').hide();
   });
 });
 
@@ -76,7 +25,7 @@ $(document).ready(function() {
 
 
 // user interface
-function infoDetails(name, email) {
+function infoDetails() {
   var form = document.forms["info"]
   var name = form["fname"].value;
   var email = form["email"].value;
